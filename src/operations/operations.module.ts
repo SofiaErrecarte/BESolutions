d363b14b0c1d115b80e8090c6cfb41803ece9598
Lola <dolores_biondo@hotmail.com>
2022-02-02 21:12:45 -0300

    operation/module

--- src/operations/operations.module.ts
+++ src/operations/operations.module.ts
@@ -7,6 +7,8 @@ import { Cart } from './entities/cart.entity';
 import { Delivery } from './entities/delivery.entity';
 import { Operation } from './entities/operation.entity';
 import { State } from './entities/state.entity';
+import { DeliveryToState } from './entities/deliveryToState.entity';
+import { OperationToState } from './entities/operationToState.entity';
 
 import { OperationsController } from './controllers/operations.controller';
 import { CartsController } from './controllers/carts.controller';
@@ -18,11 +20,39 @@ import { CartsService } from './services/carts.service';
 import { DeliveriesService } from './services/delivery.service';
 import { StateService } from './services/state.service';
 import { ProductsModule } from 'src/products/products.module';
+import { DeliveryToStateController } from './controllers/deliveryToState.controller';
+import { OperationToStateController } from './controllers/operationToState.controller';
+import { DeliveryToStateService } from './services/deliveryToState.service';
+import { OperationToStateService } from './services/operationToState.service';
 
 @Module({
-  imports: [forwardRef(()=>ProductsModule),TypeOrmModule.forFeature([Cart, Delivery, Operation, State])],
-  controllers: [OperationsController, CartsController, DeliveryController, StateController],
-  providers: [OperationsService, DeliveriesService, CartsService, StateService],
+  imports: [
+    forwardRef(() => ProductsModule),
+    TypeOrmModule.forFeature([
+      Cart,
+      Delivery,
+      Operation,
+      State,
+      DeliveryToState,
+      OperationToState,
+    ]),
+  ],
+  controllers: [
+    OperationsController,
+    CartsController,
+    DeliveryController,
+    StateController,
+    DeliveryToStateController,
+    OperationToStateController,
+  ],
+  providers: [
+    OperationsService,
+    DeliveriesService,
+    CartsService,
+    StateService,
+    DeliveryToStateService,
+    OperationToStateService,
+  ],
   exports: [OperationsService, TypeOrmModule],
 })
 export class OperationsModule {}
